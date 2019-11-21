1209ad49d946dc7775ccba4d96b7ae8f1c92e370
knvelasquez <knvelasquez35@gmail.com>
2019-11-21 10:17:57 -0300

    rotate
    rotate

--- upload/rotate.php
+++ upload/rotate.php
@@ -1,20 +1,19 @@
-<?php 
+<?php
+//Start the session
+session_start();
+ini_set('memory_limit', '1024M');
 /*error_reporting(E_ALL);
 ini_set('display_errors', 'on'); 
 echo "aaa"; 
 ob_flush();*/
-?>
-<?php
-// Start the session
-//session_start();
-
-ini_set('memory_limit', '1024M');
+header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
+header('Expires: January 01, 2013'); // Date in the past
+header('Pragma: no-cache');
 //change this to be whatever degree of rotation you want
 $degree = (isset($_POST["degree"]))?$_POST["degree"]:(isset($_GET["degree"]))?$_GET["degree"]:90;
 //this is the original file
 $filename=(isset($_POST["file"]))?$_POST["file"]:$_GET["file"];
 $filename="uploads/$filename";
-
 //if(!file_exists($filename))
 //if(!@getimagesize($filename))
 //{
@@ -24,19 +23,15 @@ $ext = pathinfo($filename, PATHINFO_EXTENSION);
 $info   = @getimagesize($filename);
 $type = $info['mime'];
 $name = '../uploads/'.time().'.'.$ext;
-
-// Tipo de contenido
-
+//Tipo de contenido
 /*if(file_exists($name))
 	{
 		$name = 'uploads/'.time().'o.'.$ext;
-	}*/
-	
+	}*/	
 if($type == 'image/jpeg')
 {
-	//header('Content-type: image/jpeg');
+	header('Content-type: image/jpeg');
 	$image=imagecreatefromjpeg($filename);
-	//$source=@imagecreatefromjpeg($filename) or notfound();
 	$rotate=@imagerotate($image,$degree,0);
 	@unlink($filename);	
 	imagejpeg($rotate,$filename); //save the new image
@@ -44,38 +39,14 @@ if($type == 'image/jpeg')
 }
 else
 {
-	$image = imagecreatefrompng($filename);	
-	
-	header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
-	header('Expires: January 01, 2013'); // Date in the past
-	header('Pragma: no-cache');
 	header("Content-Type: image/png");
-	
-	//$source = imagecreatefrompng($filename) or notfound();
+	$image = imagecreatefrompng($filename);			
 	$rotate = imagerotate($image,$degree,0);
-	@unlink($filename);	
-		
+	@unlink($filename);			
 	imagepng($rotate); //save the new image	
 	imagepng($rotate,$filename); //save the new image
-
-//base64_encode($filename);	
 }
 //free the memory
-imagedestroy($source);
-imagedestroy($rotate);	
-//$temp = substr($filename,0,19);		
-//$temp_ext = substr($filename,21);		
-/*if(file_exists($temp.'l'.$temp_ext))
-{
-	@unlink($temp.'l'.$temp_ext);
-}
-if(file_exists($temp.'m'.$temp_ext))
-{
-	@unlink($temp.'m'.$temp_ext);
-}
-if(file_exists($temp.'t'.$temp_ext))
-{
-	@unlink($temp.'t'.$temp_ext);
-}*/
-//echo $name;
+imagedestroy($image);
+imagedestroy($rotate);
 ?>
